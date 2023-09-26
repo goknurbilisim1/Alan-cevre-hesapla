0cede8aba7f60c65ad3a87cd9f332229fb927d9b
goknurbilisim1 <145705901+goknurbilisim1@users.noreply.github.com>
2023-09-26 16:13:10 +0300

    Add files via upload

---
+++ Dikdortgen-alan-cevre/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Dikdortgen_alan_cevre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.txtKisaKenar = new System.Windows.Forms.TextBox();
+            this.txtUzunKenar = new System.Windows.Forms.TextBox();
+            this.lblAlan = new System.Windows.Forms.Label();
+            this.lblCevre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(29, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kısa Kenar:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(20, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Uzun Kenar:";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(151, 94);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(115, 43);
+            this.btnHesapla.TabIndex = 2;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // txtKisaKenar
+            // 
+            this.txtKisaKenar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKisaKenar.Location = new System.Drawing.Point(151, 24);
+            this.txtKisaKenar.Name = "txtKisaKenar";
+            this.txtKisaKenar.Size = new System.Drawing.Size(115, 29);
+            this.txtKisaKenar.TabIndex = 3;
+            // 
+            // txtUzunKenar
+            // 
+            this.txtUzunKenar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUzunKenar.Location = new System.Drawing.Point(151, 59);
+            this.txtUzunKenar.Name = "txtUzunKenar";
+            this.txtUzunKenar.Size = new System.Drawing.Size(115, 29);
+            this.txtUzunKenar.TabIndex = 4;
+            // 
+            // lblAlan
+            // 
+            this.lblAlan.AutoSize = true;
+            this.lblAlan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAlan.Location = new System.Drawing.Point(147, 154);
+            this.lblAlan.Name = "lblAlan";
+            this.lblAlan.Size = new System.Drawing.Size(58, 24);
+            this.lblAlan.TabIndex = 5;
+            this.lblAlan.Text = "Alan:";
+            // 
+            // lblCevre
+            // 
+            this.lblCevre.AutoSize = true;
+            this.lblCevre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCevre.Location = new System.Drawing.Point(147, 178);
+            this.lblCevre.Name = "lblCevre";
+            this.lblCevre.Size = new System.Drawing.Size(71, 24);
+            this.lblCevre.TabIndex = 6;
+            this.lblCevre.Text = "Çevre:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(344, 243);
+            this.Controls.Add(this.lblCevre);
+            this.Controls.Add(this.lblAlan);
+            this.Controls.Add(this.txtUzunKenar);
+            this.Controls.Add(this.txtKisaKenar);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TextBox txtKisaKenar;
+        private System.Windows.Forms.TextBox txtUzunKenar;
+        private System.Windows.Forms.Label lblAlan;
+        private System.Windows.Forms.Label lblCevre;
+    }
+}
+
---
+++ Dikdortgen-alan-cevre/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dikdortgen_alan_cevre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int kisakenar, uzunkenar, alan, cevre;
+            kisakenar = Convert.ToInt32(txtKisaKenar.Text);
+            uzunkenar = Convert.ToInt32(txtUzunKenar.Text);
+            alan = uzunkenar * kisakenar;
+            cevre = (uzunkenar + kisakenar) * 2;
+
+            lblAlan.Text = "Alan: "+alan.ToString();
+            lblCevre.Text ="Çevre: "+cevre.ToString();
+
+
+
+
+            
+
+        }
+    }
+}
